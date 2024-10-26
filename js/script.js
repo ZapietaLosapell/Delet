be71350d9e0a51129542921956f208c76e80385a
ZapietaLosapell <diadiazlez@gmail.com>
2024-10-26 03:12:45 +0200

    add-captions-video

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ for (let i = 0; i < aviso.length; i++) {
       });
 }
 
+let video = document.querySelector("video");
+let track = video.addTextTrack("subtitles", "Subtitles", "en");
+track.mode = "showing";
+track.addCue(new VTTCue(0.2, 2.8, "In a world where everything is visible..."));
+track.addCue(new VTTCue(3.2, 7, "Some stories remain hidden, on the verge of extinction."));
+track.addCue(new VTTCue(8, 11.2, "Stories that hide right before our eyes."));
+track.addCue(new VTTCue(16.3, 17.9, "<b>Do you want to run it?</b>"));
+console.log(track.cues);
+
+
