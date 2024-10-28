401f693723b31ce9fe132eae238e6c5e9acdd3f3
ZapietaLosapell <diadiazlez@gmail.com>
2024-10-28 05:04:43 +0100

    modal_open-close_behavior
    Remove scroll from the modal, pause video, and hide tooltip.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerEl = document.querySelector('[data-bs-toggle="tooltip"]');
+const tooltip = new bootstrap.Tooltip(tooltipTriggerEl);
+
+tooltipTriggerEl.addEventListener("mouseleave", () => {
+  tooltip.hide();
+});
+
+
+// Aviso
 
 const close = document.getElementById("close");
 const aviso = document.getElementsByClassName("aviso");
-for (let i = 0; i < aviso.length; i++) {
+
+for (let i = 0; i < aviso.length; i++) {  // Nota: Para todas las .aviso, opacidad 0.
     close.addEventListener("click", function(){
         aviso[i].style.opacity = "0";
         setTimeout(() => {
@@ -12,15 +20,40 @@ for (let i = 0; i < aviso.length; i++) {
     });
 }
 
+
+// Subtítulos
+
 let videos = document.querySelectorAll("video");
 videos.forEach(video => {
-  let track = video.addTextTrack("subtitles", "Subtitles", "en");
-  track.mode = "showing";
-  track.addCue(new VTTCue(0.2, 2.8, "In a world where everything is visible..."));
-  track.addCue(new VTTCue(3.2, 7, "Some stories remain hidden, on the verge of extinction."));
-  track.addCue(new VTTCue(8, 11.2, "Stories that hide right before our eyes."));
-  track.addCue(new VTTCue(13, 14.6, "<b>Y¿Delet.exe</b>"));
-  track.addCue(new VTTCue(16, 17.9, "<b>Do you want to run it?</b>"));
+
+    let track = video.addTextTrack("subtitles", "Subtitles", "en");
+    track.mode = "showing";
+    track.addCue(new VTTCue(0.2, 2.8, "In a world where everything is visible..."));
+    track.addCue(new VTTCue(3.2, 7, "Some stories remain hidden, on the verge of extinction."));
+    track.addCue(new VTTCue(8, 11.2, "Stories that hide right before our eyes."));
+    track.addCue(new VTTCue(13, 14.6, "<b>Y¿Delet.exe</b>"));
+    track.addCue(new VTTCue(16, 17.9, "<b>Do you want to run it?</b>"));
+
 console.log(track.cues);
 });
 
+
+// Video de la ventana modal
+
+const modal = document.getElementById("modal1");
+const video = document.getElementById("teaser");
+
+modal.addEventListener("close", () => {
+    tooltip.hide();
+});
+
+window.openModal = function() {
+    modal.showModal();
+    document.body.classList.add('no-scroll');
+};
+
+window.closeModal = function() {
+    modal.close();
+    video.pause();
+    document.body.classList.remove('no-scroll');
+};
