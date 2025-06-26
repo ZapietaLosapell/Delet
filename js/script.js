0ecbbd996c3df13c3bd31f83114a6c12b1d57b56
ZapietaLosapell <diadiazlez@gmail.com>
2025-06-26 14:07:05 +0200

    Habilitar modal

--- js/script.js
+++ js/script.js
@@ -69,19 +69,19 @@ fixed2.addEventListener("mouseout", function() {
 
 // Subtítulos
 
-let videos = document.querySelectorAll(".teaser");
-videos.forEach(video => {
+// let videos = document.querySelectorAll(".teaser");
+// videos.forEach(video => {
 
-    let track = video.addTextTrack("subtitles", "Subtitles", "en");
-    track.mode = "showing";
-    track.addCue(new VTTCue(0.2, 2.8, "In a world where everything is visible..."));
-    track.addCue(new VTTCue(3.2, 7, "Some stories remain hidden, on the verge of extinction."));
-    track.addCue(new VTTCue(8, 11.2, "Stories that hide right before our eyes."));
-    track.addCue(new VTTCue(13, 14.6, "<b>Y¿Delet.exe</b>"));
-    track.addCue(new VTTCue(16, 17.9, "<b>Do you want to run it?</b>"));
+//     let track = video.addTextTrack("subtitles", "Subtitles", "en");
+//     track.mode = "showing";
+//     track.addCue(new VTTCue(0.2, 2.8, "In a world where everything is visible..."));
+//     track.addCue(new VTTCue(3.2, 7, "Some stories remain hidden, on the verge of extinction."));
+//     track.addCue(new VTTCue(8, 11.2, "Stories that hide right before our eyes."));
+//     track.addCue(new VTTCue(13, 14.6, "<b>Y¿Delet.exe</b>"));
+//     track.addCue(new VTTCue(16, 17.9, "<b>Do you want to run it?</b>"));
 
-console.log(track.cues);
-});
+// console.log(track.cues);
+// });
 
 // Video de la ventana modal
 
